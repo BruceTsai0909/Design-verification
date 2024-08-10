404bf3f032da82e6d99e9250e8fab6cc4fc13587
BruceTsai0909 <Whitedragon0909@gmail.com>
2024-08-10 13:46:11 +0800

    monitor done

--- UVM_Combinational_Adder/testbench.sv
+++ UVM_Combinational_Adder/testbench.sv
@@ -76,3 +76,36 @@ endtask
 
 endclass
 /////////////////////////////////////////////////
+class monitor extends uvm_monitor;
+`uvm_component_utils(monitor)
+
+uvm_analysis_port #(transaction) send;
+
+function new(input string inst = "monitor", uvm_component parent = bull);
+    super.new(path, parent);
+    send = new("send", this);
+endfunction
+
+transaction t;
+virtual add_if aif;
+
+virtual function build_phase(uvm_phase phase);
+super.build_phase(phase);
+t = transaction::type_id::create("t");
+if(!uvm_config_db #(virtual add_if)::get(this,,"aif", aif))
+    `uvm_error("MON", "Unable to access uvm_config_db");
+endfunction
+
+virtual task run_phase(uvm_phase phase);
+forever begin
+#10;
+t.a = aif.a;
+t.b = aif.b;
+t.y = aif.y;
+`uvm_info("MON", $sformatf("Data send to scoreboard a : %0d, b : %0d, y : %0d", t.a, t.b, t.y), UVM_NONE);
+send.write(t); //send data to scoreboard
+end
+endtask
+
+endclass
+/////////////////////////////////////////////////
